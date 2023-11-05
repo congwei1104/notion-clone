22ed4c964e41c4f451f31fe9a002166f4b1c1971
congwei1104 <congwei_huang@163.com>
2023-11-05 14:54:22 +0800

    添加新特性：登录授权

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { Spinner } from '@/components/spinner';
 import { Button } from '@/components/ui/button';
+import { useConvexAuth } from 'convex/react';
 import { ArrowRight } from 'lucide-react';
+import { SignInButton } from '@clerk/clerk-react';
+import Link from 'next/link';
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className=" text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,10 +20,27 @@ const Heading = () => {
         Hotion is the connected workspace where <br />
         better, faster work happens.
       </h3>
-      <Button>
-        Enter Hotion
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Hotion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Hotion free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://worthy-chow-38.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
