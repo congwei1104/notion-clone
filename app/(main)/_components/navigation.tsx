923bd8142404a5d1483b0621f809f668cc51c4fc
congwei1104 <congwei_huang@163.com>
2023-11-06 21:04:17 +0800

    添加用户设置

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, Menu } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
+import UserItem from './user-item';
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -112,7 +113,7 @@ const Navigation = () => {
           <ChevronsLeft />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
