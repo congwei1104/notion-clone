fe7e2dde13c662208aae564d5a9b2b55876be772
congwei1104 <congwei_huang@163.com>
2023-11-08 01:38:48 +0800

    增加回收站功能

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,15 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import { ChevronsLeft, Menu, PlusCircle, Search, Settings } from 'lucide-react';
+import {
+  ChevronsLeft,
+  Menu,
+  Plus,
+  PlusCircle,
+  Search,
+  Settings,
+  Trash,
+} from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
@@ -9,9 +17,15 @@ import UserItem from './user-item';
 import Item from './item';
 import { useMutation } from 'convex/react';
 import { api } from '@/convex/_generated/api';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover';
 
 import { toast } from 'sonner';
 import DocumentList from './document-list';
+import TrashBox from './trash-box';
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -137,6 +151,18 @@ const Navigation = () => {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+          <Popover>
+            <PopoverTrigger className=" w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              className=" p-0 w-72"
+              side={isMobile ? 'bottom' : 'right'}
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
         <div
           onMouseDown={handleMouseDown}
